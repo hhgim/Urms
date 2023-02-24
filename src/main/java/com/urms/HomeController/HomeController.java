abf276296d64b68eb802c6c678b0874e9d7562d2
hhgim <96dkrl@naver.com>
2023-02-24 14:10:22 +0900

    컨트롤러 및 인덱스 페이지 테스트

---
+++ src/main/java/com/urms/HomeController/HomeController.java
@@ -0,0 +1,18 @@
+package com.urms.HomeController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView home() {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home");
+		return mv;
+	}
+
+}
